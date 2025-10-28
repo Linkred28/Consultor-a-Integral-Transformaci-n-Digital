55bd7ebcb428feb31c29009e7a9f52e1eec266dd
Linkred28 <areliaguilarln@gmail.com>
2025-10-27 22:43:57 -0600

    feat: Add placeholder for chatbot component
    Introduces a basic, empty React component for a chatbot, intended to be integrated later.
    
    Updates a video URL in the constants file.

---
+++ components/Chatbot.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Chatbot: React.FC = () => {
+    return (
+        <div>
+            {/* Chatbot placeholder */}
+        </div>
+    );
+};
+
+export default Chatbot;

a9cd65baa31ab4894b9936760f2dfe6b31cdc352
Linkred28 <areliaguilarln@gmail.com>
2025-12-03 22:12:41 -0600

    Updated Preloader.tsx

--- components/Preloader.tsx
+++ components/Preloader.tsx
@@ -38,7 +38,7 @@ const Preloader: React.FC<PreloaderProps> = ({ isVisible }) => (
         }}
       >
         {/* Logo más grande que el texto */}
-        <Logo className="w-24 h-24" />
+        <Logo className="w-26 h-26" />
         <span className="text-2xl sm:text-3xl font-bold tracking-tight uppercase">
           METODIKO
         </span>
